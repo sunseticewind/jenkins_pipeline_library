99e9021fea955c36d07cfc94d9dfb94949672a43
sunseticewind <104580146+sunseticewind@users.noreply.github.com>
2022-04-29 03:22:06 +0800

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,5 +1,8 @@
-def call(params){
+import org.foo.testBar
+
+def call(x,y,z){
     withEnv(['PATH+EXTRA=/usr/sbin:/usr/bin:/sbin:/bin']){
-        sh "echo ${params}"
+        def tb = new testBar(x, y, z)
+        sh "echo ${tb.volume()}"
     }
 }
