b91a0bb8389df07a5b4d6298e74c4dfcca8bc715
sunseticewind <104580146+sunseticewind@users.noreply.github.com>
2022-04-29 03:27:43 +0800

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -2,10 +2,7 @@ import org.foo.testBar
 
 def call(x,y,z){
     withEnv(['PATH+EXTRA=/usr/sbin:/usr/bin:/sbin:/bin']){
-        def tb = new testBar()
-        tb.setLength(x)
-        tb.setWidth(y)
-        tb.setHeight(z)
+        def tb = new testBar(x,y,z)
         sh "echo ${tb.volume()}"
     }
 }
