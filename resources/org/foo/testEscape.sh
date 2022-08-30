4b6455c54b62b4b3e645b57d7a6012f4d979dfa2
hank_cheng <hank_cheng@htc.com>
2022-08-30 17:12:57 +0800

    test

--- resources/org/foo/testEscape.sh
+++ resources/org/foo/testEscape.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set +ex
-scp -r -o StrictHostKeyChecking=no -i $1 command.sh $2@$3:$WORKSPACE
+scp -r -o StrictHostKeyChecking=no -i $1 $WORKSPACE/command.sh $2@$3
 ssh -o StrictHostKeyChecking=no -tt -i $1 $2@$3 bash command.sh | tee test.txt
