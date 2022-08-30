9d54046d9dd55cfb7a6c42b461ad37401d75f4ef
hank_cheng <hank_cheng@htc.com>
2022-08-30 16:04:32 +0800

    test

--- vars/testEscape.groovy
+++ vars/testEscape.groovy
@@ -13,7 +13,7 @@ def call(server_ip, exec_command) {
                     folderPath+=folderPathList[i]+"/"
                 }
                 sh "chmod +x ${folderPath}resources/org/foo/testEscape.sh"
-                def SSH_COMMAND = "${folderPath}resources/org/foo/testEscape.sh '${SSH_KEY}' ${SSH_USER} ${server_ip} '${exec_command}' ${env.BUILD_ID}"
+                def SSH_COMMAND = "${folderPath}resources/org/foo/testEscape.sh '${SSH_KEY}' ${SSH_USER} ${server_ip} '${exec_command}' ${env.BUILD_ID}.txt"
                 
                 rtnStatus = sh (
                     script: SSH_COMMAND,
