e5b2f07dc36821a5055c0758ab9c3a74a4c8dc69
hank_cheng <hank_cheng@htc.com>
2022-09-22 11:13:57 +0800

    check server test

--- vars/checkServer.groovy
+++ vars/checkServer.groovy
@@ -2,8 +2,7 @@ import groovy.json.*
 
 def call(){
     json_str = libraryResource 'org/foo/checkServerList.json'
-    jsonSlurperClassic = new JsonSlurperClassic()
-    json_arr = jsonSlurperClassic.parseText(json_str)
+    json_arr = readJson(json_str)
     failed_list = []
     for(json_obj in json_arr){
         try{
