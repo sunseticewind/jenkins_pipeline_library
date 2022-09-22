baffabf6a950275ec51f6ff2d2dba5d4e8f39098
hank_cheng <hank_cheng@htc.com>
2022-09-22 11:23:09 +0800

    check server test

--- vars/checkServer.groovy
+++ vars/checkServer.groovy
@@ -5,15 +5,16 @@ def call(){
     json_arr = readJSON(json_str)
     failed_list = []
     for(json_obj in json_arr){
-        try{
-            for (port in json_obj["PORT"]){
+        for (port in json_obj["PORT"]){
+            try{
                 cmd = "nc -vz ${json_obj["HOST"]} ${port} -w 1"
                 sh (cmd)
+            }catch(Exception e){
+                is_failed=true
+                failed_list.add(json_obj["HOST"])
             }
-        }catch(Exception e){
-            is_failed=true
-            failed_list.add(json_obj["HOST"])
         }
+        
     }
-    return failed_list
+    println(failed_list)
 }
