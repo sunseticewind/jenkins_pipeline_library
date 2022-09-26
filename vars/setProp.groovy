8dc32a8e44ca504353dadb6894c33470aef685da
hank_cheng <hank_cheng@htc.com>
2022-09-26 16:41:50 +0800

    test set prop

--- vars/setProp.groovy
+++ vars/setProp.groovy
@@ -1,5 +1,6 @@
 def call() {
-  properties([
-    buildDiscarder(logRotator(numToKeepStr: '100'))
-  ])
+    properties([
+        buildDiscarder(logRotator(numToKeepStr: '100'))
+        timeout(time: 10, unit: 'MINUTES')
+    ])
 }
