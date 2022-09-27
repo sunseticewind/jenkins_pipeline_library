bfdbc7addfc98260396d2142a85f1668b190be88
hank_cheng <hank_cheng@htc.com>
2022-09-27 15:46:12 +0800

    test

--- vars/setJobByInstance.groovy
+++ vars/setJobByInstance.groovy
@@ -1,6 +1,6 @@
 def call(){
     def item = Jenkins.instance.getItemByFullName(env.JOB_NAME) 
-    item.setConcurrentBuild(true)
+    item.setConcurrentBuild(false)
     item.setDisabled(true)
     item.save()
 }
