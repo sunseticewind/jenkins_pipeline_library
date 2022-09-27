0197d9739bb4ab9895e82cc79e968eef35936d1d
hank_cheng <hank_cheng@htc.com>
2022-09-27 17:29:07 +0800

    test

--- vars/setJobByInstance.groovy
+++ vars/setJobByInstance.groovy
@@ -7,7 +7,12 @@ def call(isConcurrentBuild, isOverwriteBD){
         item.setBuildDiscarder(new LogRotator(-1,100,-1,-1));
     }
     item.save()
-    timeout(time: 30, unit: 'MINUTES'){
-        echo 'set default timeout duration'
+    try {
+        timeout(time: 30, unit: 'MINUTES'){
+            echo 'set default timeout duration'
+        }
+    } catch(err) {
+        echo 'test'
     }
+    
 }
