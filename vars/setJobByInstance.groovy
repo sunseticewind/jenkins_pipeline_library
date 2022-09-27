7eb9dd88b330d961534e6b480b7c39560d33ce23
hank_cheng <hank_cheng@htc.com>
2022-09-27 16:37:54 +0800

    test

--- vars/setJobByInstance.groovy
+++ vars/setJobByInstance.groovy
@@ -1,9 +1,10 @@
 import hudson.tasks.LogRotator
 
-def call(){
+def call(isConcurrentBuild, isOverwriteBD){
     def item = Jenkins.instance.getItemByFullName(env.JOB_NAME) 
-    item.setConcurrentBuild(false)
-    // item.setDisabled(true)
-    item.setBuildDiscarder(new LogRotator(-1,100,-1,-1));
+    item.setConcurrentBuild(isConcurrentBuild)
+    if(isOverwriteBD){
+        item.setBuildDiscarder(new LogRotator(-1,100,-1,-1));
+    }
     item.save()
 }
